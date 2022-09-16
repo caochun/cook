f0120d90629d75cc1833f19e3b48b77744c1db55
Chun <caochun@gmail.com>
2022-09-16 13:25:14 +0800

    remove gtm

--- src/components/tags/DishTag.vue
+++ src/components/tags/DishTag.vue
@@ -1,25 +1,9 @@
 <script lang="ts" setup>
-import { useGtm } from '@gtm-support/vue-gtm'
 import { tools } from '~/data/food'
 import type { RecipeItem } from '~/types'
 defineProps<{
   dish: RecipeItem
 }>()
-
-const gtm = useGtm()
-
-const triggerGtm = (val: string) => {
-  gtm?.trackEvent({
-    event: 'click',
-    category: `dish_${val}`,
-    action: 'click_recipe',
-    label: '跳转菜谱',
-  })
-  gtm?.trackEvent({
-    event: 'click_dish',
-    action: val,
-  })
-}
 </script>
 
 <template>
@@ -27,7 +11,6 @@ const triggerGtm = (val: string) => {
     :href="dish.link || `https://www.bilibili.com/video/${dish.bv}`" target="_blank" class="dish-tag tag rounded" p="x-2"
     border="~ blue-200 dark:blue-800"
     bg="blue-300 opacity-20"
-    @click="triggerGtm(dish.name)"
   >
     <span m="r-1" class="inline-flex justify-center items-center" text="sm blue-700 dark:blue-200">
       {{ `${dish.tags?.includes('杂烩') ? '🍲' : dish.emojis.join(' ')} ${dish.name}` }}
