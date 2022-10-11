d1a1424174d3ea89e5019d4ef46b3e1e975b1061
Chun <caochun@gmail.com>
2022-10-11 11:34:25 +0800

    update:

--- src/components/BaseFooter.vue
+++ src/components/BaseFooter.vue
@@ -6,7 +6,7 @@
     </div>
 
     <div m="t-2" class="inline-flex justify-center items-center" text="xs">
-      <span>Designed By <a href="https://www.instagram.com/catherine.eats/">Catherine Lu</a>
+      <span>Designed By Catherine Lu  <a href="https: www.instagram.com/catherine.eats/">@catherine.eats</a>
       </span>
     </div>
 
@@ -16,10 +16,10 @@
     </div>
 
     <div m="t-2" class="inline-flex justify-center items-center" text="xs">
-      美食每刻是上海民办位育中学美食类社团，由一群热爱美食、热爱健康生活的高中生组成。我们会尝试制作各种美食，学习相关营养知识。在疫情期间，美食每刻的同学们也没闲着，在家研发菜谱。利用现有的食材（土豆，胡萝卜，番茄等等）这些食材很容易吃腻，或者不知道怎么做好吃。美食社由此设计出一款小程序，根据现有的食材搭配出营养好吃的菜谱。
+      美食每刻是上海民办位育中学美食类社团，由一群热爱美食、热爱健康生活的高中生组成。我们会尝试制作各种美食，学习相关营养知识。在疫情期间，美食每刻的同学们也没闲着，在家研发菜谱。利用现有的食材（土豆，胡萝卜，番茄等等）这些食材很容易吃腻，或者不知道怎么做好吃。美食社由此设计出一款小程序，根据现有的食材搭配出营养、健康、便利、好吃的菜谱。
     </div>
     <div m="t-2" class="inline-flex justify-center items-center" text="xs">
-      Weiyu food club is a group of high school foodie who love food and enjoy a healthy lifestyle. The club tries and makes various dishes, learns about nutritional information. In 2022 spring, Shanghai was lockdown. Club members designed a WeChat mini program to help people decide what and how to eat using limited ingredients.
+      Weiyu food club is a group of high school foodie who love food and enjoy a healthy lifestyle. The club tries and makes various dishes, learns about nutritional information. In 2022 spring, Shanghai was lockdown. Club members designed a WeChat mini program to help people decide what and how to eat using limited ingredients. We aims to guide people to eat healthy and nutrional during lockdown with convenient recipes.
     </div>
   </div>
 </template>
